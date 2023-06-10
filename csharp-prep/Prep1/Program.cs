35c80a2ce4384e6d162b3a6c6638e7709dac95c7
studentwdd <chebilkentop@yahoo.com>
2023-06-10 23:12:46 +0800

    Initial commit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,10 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
         Console.Write("What is your first name?");
-        string firstname = Console.ReadLine();
+        string first = Console.ReadLine();
+
         Console.Write("What is your last name?");
-        string lastname = Console.ReadLine();
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
         
     }
 }
