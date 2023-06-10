11122d9a529b3a600b20b4ded514f7475cd0072c
studentwdd <chebilkentop@yahoo.com>
2023-06-11 00:10:10 +0800

    Initial commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,14 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-        
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
-        
-        Console.WriteLine($"Your name is {last}, {first} {last}.");
-    }
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Make better next time!");
+        }
+    }    
 }
