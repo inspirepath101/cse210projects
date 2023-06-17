424caae6e358d4386073a5f84da1be648912e6f5
studentwdd <chebilkentop@yahoo.com>
2023-06-18 07:38:06 +0800

    Entrycs

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,14 @@
+Who was the most interesting person I interacted with today?
+What was the best part of my day?
+How did I see the hand of the Lord in my life today?
+What was the strongest emotion I felt today?
+If I had one thing I could do over today, what would it be?
+
+public class Entry
+{
+    public string _Write;
+    public string _Display;
+    public string _Load;
+    public string _Save;
+    public string _Quit;
+}
