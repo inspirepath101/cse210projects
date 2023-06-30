80105237e057469c02b7b2b8f3aff96488bc6193
studentwdd <chebilkentop@yahoo.com>
2023-06-30 13:38:42 +0800

    L04Programcs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+         
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Angelic Molina", "Division");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Daile Franco", "Fractions", "8.2", "3-15");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Hillary Maughan", "Philippines History", "National Heroes");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
+    
+    
 }
