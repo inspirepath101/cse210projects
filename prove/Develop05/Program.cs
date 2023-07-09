1721668a1504dc5f0d77cfb29dfc4dad41f4a699
studentwdd <chebilkentop@yahoo.com>
2023-07-09 13:52:19 +0800

    Prgrmcsw05

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,5 +5,25 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop05 World!");
+        List<Goal> goal = new List<Goal>();
+
+        SimpleGoals g1 = new SimpleGoals("");
+        goal.Add(g1);
+
+        CheckListGoals g2 = new CheckListGoals("");
+        goal.Add(g2);
+
+        EternalGoals g3 = new EternalGoals("");
+        goal.Add(g3)
+
+        foreach (Goal g in goals)
+        {
+            string goal = g.GetGoal();
+
+            Console.WriteLine($"")
+        }
+
+
     }
+    
 }
