e87c6d196a05048c438d7b0209bc2734fc4ef3f2
studentwdd <chebilkentop@yahoo.com>
2023-07-09 13:54:57 +0800

    Goalcs5

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,13 @@
+public class Goal
+{
+    private string _shortname;
+    private string _description;
+    private string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        
+    } 
+
+
+}
