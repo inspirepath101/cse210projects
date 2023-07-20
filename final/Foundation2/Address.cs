97baac3106c1614f0097b7256209b873a889c7c8
studentwdd <chebilkentop@yahoo.com>
2023-07-20 13:07:36 +0800

    F2Addresscs

---
+++ final/Foundation2/Address.cs
@@ -0,0 +1,18 @@
+ public class Address
+{
+    private string _street;
+   
+   
+
+    public string GetStreet()
+    {
+        return _street;
+    }
+
+    public void Setaddress(string street)
+    {
+        _street = street;
+    }
+
+
+}
