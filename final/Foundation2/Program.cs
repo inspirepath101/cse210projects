fdfcf490c4fddad24e5afa7820e73a7909d6f3ec
studentwdd <chebilkentop@yahoo.com>
2023-07-20 13:14:13 +0800

    F2Programcs

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Console.WriteLine("Hello Foundation2 World!");  
     }
+public class Customer
+{
+    private string _name;
+    private string _address;
+    
+    public string GetCustomerInfo()
+    {
+        return "Thanks, " + _name;
+    }
+
+
+    public string GetCustomerInfo()
+    {
+        return "Sincerely, " + GetFullName();
+    }
+
+    private string GetFullName()
+    {
+        return _name + " " + _address;
+    }
+   
+}
 }
