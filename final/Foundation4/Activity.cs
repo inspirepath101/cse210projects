f519d3ca1d2b4db255f1ca9115bcd8cb5784bb85
studentwdd <chebilkentop@yahoo.com>
2023-07-20 14:20:19 +0800

    F4Activity

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,15 +1,18 @@
+
+using System;
 public class Activity
 {
-   List<Activity> activities = new List<Activity>();
-
-
-     activity.add(new Activity());
-     activity.add(new LengthActivity());
-
-
-     foreach(Activity activity in activities)
-{
-     float minutes = activity.Minutes();
-     Console.WriteLine(minutes);
-} 
+    static void activity(string[] args)
+    {
+        for (int i = 5; i > 0; i --)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write("\b\b");
+        }
+        Console.WriteLine(".");
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(10);
+        Console.WriteLine("Done");
+    }
 }
