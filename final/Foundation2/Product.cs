ad3236626fb55f77a6342955eea964f288acb255
studentwdd <chebilkentop@yahoo.com>
2023-07-20 13:09:29 +0800

    F2Productcs

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,18 @@
+public class Product
+{
+    private string _brand;
+    private int _quantity;
+   
+
+    public string GetBrand()
+    {
+        return _brand;
+    }
+
+    public void SetBrand(string brand)
+    {
+        _brand = brand;
+    }
+
+
+}
