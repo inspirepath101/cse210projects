034c2d638ebe624bd64651f9f1a73d79805cc350
studentwdd <chebilkentop@yahoo.com>
2023-07-20 13:08:22 +0800

    F2Customercs

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,18 @@
+public class Customer
+{
+    private string _name;
+    private string _address;
+   
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+
+}
